607d2d71e2bb40b2d9708ac04a4e79ec6ebaba70
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 19:00:57 +0300

    added to looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
-
-def square_integers(int_list):
-    # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
